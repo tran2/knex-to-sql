e4aa703952dfe156948244131fdf64096ecaa9b7
Tran Tu <tran.tu@zumepizza.com>
2018-05-20 16:45:01 -0700

    fix webpack production build

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
   }
 };
